1dcd21287fd74f4d172c78f383705d0eb0879bfa
K <Akarfire@gmail.com>
2025-11-14 21:14:00 +0300

    [DEV] ElementDataStructure initial commit

---
+++ Source/ElementDataStructure.py
@@ -0,0 +1,7 @@
+
+# This structure-class contains scanned data about your code
+# It will later be used to generate documentation
+class ElementDataStructure:
+    
+    def __init__(self):
+        pass
