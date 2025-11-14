2c29242170df052701f1cbcd33889631af890f39
K <Akarfire@gmail.com>
2025-11-14 21:14:09 +0300

    [DEV] FileRulesets initial commit

---
+++ Source/FileRulesets.py
@@ -0,0 +1,12 @@
+
+
+class FileRulesetStorage:
+    
+    def __init__(self, in_core):
+        self.core = in_core
+    
+    
+    # Whether there is a suitable ruleset for the specified file
+    def file_has_ruleset(self, file_name : str):
+        
+        return True
